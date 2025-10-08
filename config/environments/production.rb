8d0c7da4c9af7e009c696c67387fbc91a9f577fa
Harmony UK <gensec@harmonyuk.org>
2025-10-08 18:11:04 +0000

    Almost nailed the oauth2 connection issue. Now just have to fix REST call.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = %w(debug info warn error fatal).include?(ENV['RAILS_LOG_LEVEL']) ? ENV['RAILS_LOG_LEVEL'] : :info
+  # config.log_level = %w(debug info warn error fatal).include?(ENV['RAILS_LOG_LEVEL']) ? ENV['RAILS_LOG_LEVEL'] : :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -98,6 +98,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  config.log_level = :debug
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
